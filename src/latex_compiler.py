45ce75be29be16c268e182b88907f9cc668352b2
BagelDogM <bageldogm@gmail.com>
2025-04-09 14:21:51 +0100

    Added character escaping for special latex characters

--- src/latex_compiler.py
+++ src/latex_compiler.py
@@ -1,10 +1,27 @@
 import shutil
 
+def latex_escaped_string(string):
+    escape_characters = {
+        '\\': '\\textbackslash',
+        '{': '\\{',
+        '}': '\\}',
+        '%': '\\%'}
+    for pattern, replacement in escape_characters.items():
+        string = string.replace(pattern, replacement)
+
+    return string
+    print(f'Esc as {string}')
+
+def write_element(file_object, element):
+    # Escape all characters for each entry after the element type so they aren't compiled wrong - e.g. % starting a comment
+    escaped_values = map(latex_escaped_string, element['values'])
+    file_object.write(f'\\{element["type"]}'+'{'+'}{'.join(escaped_values)+'}\n')
+
 def complile_to_latex(data: list[dict]):
     # Create a copy of the template to write to in order to create the final latex
-    shutil.copy('src/template.txt', 'src/tmp.txt')
+    shutil.copy('src/template.tex', 'src/tmp.tex')
 
-    with open('src/tmp.txt', 'a') as file:
+    with open('src/tmp.tex', 'a') as file:
         # Add all trials, places and visits in order: visits, trials, places
         file.write('\\begin{offers}\n')
 
@@ -13,14 +30,14 @@ def complile_to_latex(data: list[dict]):
         relevant_elements.sort(key=lambda e: types.index(e['type'])) # Sort in the order the 'types' list is in.
 
         for element in relevant_elements:
-            file.write(f'\\{element["type"]}'+'{'+'}{'.join(element['values'])+'}\n')
+            write_element(file, element)
 
         file.write('\\end{offers}\n')
 
         # Add all other points
         relevant_elements = [e for e in data if e['type'] == 'point']
         for element in relevant_elements:
-            file.write(f'\\{element["type"]}'+'{'+'}{'.join(element['values'])+'}\n')
+            write_element(file, element)
 
         # End enumerate and document tags
         file.write('\\end{enumerate}\n\\end{document}')
